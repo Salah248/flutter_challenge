c3aafdc352c19d56c24f799334ebe843c71391be
Salah248 <salahsadoon248@gmail.com>
2025-04-07 23:21:17 +0200

    Refactor GroceryList and NewItem widgets to manage grocery items with state; implement item addition and deletion functionality.

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_challenge/data/dummy_items.dart';
+import 'package:flutter_challenge/models/grocery_item.dart';
 import 'package:flutter_challenge/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
 
   @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  final List<GroceryItem> _groceryItems = [];
+
+  @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text(
+        'No items added yet!',
+        style: TextStyle(color: Colors.white, fontSize: 20),
+      ),
+    );
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (context, index) {
+          return Dismissible(
+            key: ValueKey(_groceryItems[index]),
+            background: Container(
+              color: Theme.of(context).colorScheme.error,
+              alignment: Alignment.centerRight,
+              padding: const EdgeInsets.only(right: 20),
+              child: const Icon(Icons.delete, color: Colors.white),
+            ),
+            direction: DismissDirection.endToStart,
+            onDismissed: (direction) {
+              setState(() {
+                _groceryItems.removeAt(index);
+              });
+            },
+            child: ListTile(
+              title: Text(_groceryItems[index].name),
+              leading: Container(
+                width: 24,
+                height: 24,
+                color: _groceryItems[index].category.color,
+              ),
+              trailing: Text(_groceryItems[index].quantity.toString()),
+            ),
+          );
+        },
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Grocery List'),
@@ -14,27 +59,22 @@ class GroceryList extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.add),
             onPressed: () {
-              Navigator.of(
-                context,
-              ).push(MaterialPageRoute(builder: (context) => const NewItem()));
+              Navigator.of(context)
+                  .push<GroceryItem>(
+                    MaterialPageRoute(builder: (context) => const NewItem()),
+                  )
+                  .then((value) {
+                    if (value != null) {
+                      setState(() {
+                        _groceryItems.add(value);
+                      });
+                    }
+                  });
             },
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(groceryItems[index].name),
-            leading: Container(
-              width: 24,
-              height: 24,
-              color: groceryItems[index].category.color,
-            ),
-            trailing: Text(groceryItems[index].quantity.toString()),
-          );
-        },
-      ),
+      body: content,
     );
   }
 }
--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_challenge/data/categories.dart';
+import 'package:flutter_challenge/models/category.dart';
+import 'package:flutter_challenge/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -9,6 +11,10 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItem> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  var _enteredName = '';
+  var _enteredQuantity = 0;
+  var _selectedCategory = categories[Categories.fruit]!;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,12 +22,17 @@ class _NewItemState extends State<NewItem> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               TextFormField(
                 maxLength: 20,
                 style: const TextStyle(fontSize: 16, color: Colors.white),
+                onSaved: (newValue) {
+                  // Save the name of the item
 
+                  _enteredName = newValue!;
+                },
                 validator: (value) {
                   if (value == null ||
                       value.isEmpty ||
@@ -40,13 +51,18 @@ class _NewItemState extends State<NewItem> {
                     child: TextFormField(
                       style: const TextStyle(fontSize: 16, color: Colors.white),
                       keyboardType: TextInputType.number,
+                      onSaved: (newValue) {
+                        // Save the quantity of the item
+
+                        _enteredQuantity = int.parse(newValue!);
+                      },
                       initialValue: '1',
                       validator: (value) {
                         if (value == null ||
                             value.isEmpty ||
-                            value.trim().length <= 1 ||
-                            value.trim().length > 20) {
-                          return 'Please enter a valid Quantity';
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return 'Please enter a valid Quantity, greater than 0';
                         }
                         return null;
                       },
@@ -56,8 +72,8 @@ class _NewItemState extends State<NewItem> {
                   const SizedBox(width: 12),
                   Expanded(
                     child: DropdownButtonFormField(
+                      value: _selectedCategory,
                       style: const TextStyle(fontSize: 16, color: Colors.white),
-
                       items: [
                         for (final category in categories.entries)
                           DropdownMenuItem(
@@ -75,7 +91,11 @@ class _NewItemState extends State<NewItem> {
                             ),
                           ),
                       ],
-                      onChanged: (value) {},
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedCategory = value!;
+                        });
+                      },
                     ),
                   ),
                 ],
@@ -86,13 +106,27 @@ class _NewItemState extends State<NewItem> {
                 children: [
                   TextButton(
                     onPressed: () {
-                      Navigator.of(context).pop();
+                      _formKey.currentState?.reset();
                     },
                     child: const Text('Reset'),
                   ),
                   const SizedBox(width: 12),
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        Navigator.pop(
+                          context,
+                          // Pass the Data of the new item to the previous screen
+                          GroceryItem(
+                            id: DateTime.now().toString(),
+                            name: _enteredName,
+                            quantity: _enteredQuantity,
+                            category: _selectedCategory,
+                          ),
+                        );
+                      }
+                    },
                     child: const Text('Add Item'),
                   ),
                   const SizedBox(width: 12),
