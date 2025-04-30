4aa6069b5611740b73325ea5fdfc511f8b701d55
Salah248 <salahsadoon248@gmail.com>
2025-04-30 18:58:17 +0300

    Add full map functionality to PlacesDetails screen; implement map display and navigation to detailed location view.

--- lib/screens/places_details.dart
+++ lib/screens/places_details.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_challenge/models/place.dart';
+import 'package:flutter_challenge/screens/maps_screen.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class PlacesDetails extends StatelessWidget {
   const PlacesDetails({super.key, required this.place});
 
   final Place place;
 
+  void _openFullMap(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder:
+            (ctx) => MapScreen(
+              initialLocation: LatLng(
+                place.location.latitude,
+                place.location.longitude,
+              ),
+              isSelecting: false, // لأننا لا نريد اختيار موقع جديد
+            ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final location = LatLng(place.location.latitude, place.location.longitude);
+
     return Scaffold(
       appBar: AppBar(title: Text(place.name)),
       body: Stack(
@@ -22,17 +42,70 @@ class PlacesDetails extends StatelessWidget {
             bottom: 0,
             left: 0,
             right: 0,
-            child: Container(
-              padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 40),
-              color: Colors.black.withAlpha((255 * .42).round()),
-              child: Text(
-                place.location.address,
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundColor: Colors.white,
+                  child: ClipOval(
+                    child: SizedBox(
+                      width: 90,
+                      height: 90,
+                      child: FlutterMap(
+                        options: MapOptions(
+                          onTap: (tapPosition, point) => _openFullMap(context),
+                          initialCenter: location,
+                          initialZoom: 13,
+                          interactionOptions: const InteractionOptions(
+                            flags: InteractiveFlag.none,
+                          ),
+                        ),
+                        children: [
+                          TileLayer(
+                            urlTemplate:
+                                'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                          ),
+                          MarkerLayer(
+                            markers: [
+                              Marker(
+                                point: location,
+                                width: 30,
+                                height: 30,
+                                child: Icon(
+                                  Icons.location_pin,
+                                  size: 20,
+                                  color: Theme.of(context).colorScheme.primary,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
-              ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 16,
+                    horizontal: 24,
+                  ),
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Colors.transparent, Colors.black54],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Text(
+                    place.location.address,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
