5f8907c6a2f6c6015a107f1a1f03e4e74ea162e5
Salah248 <salahsadoon248@gmail.com>
2025-05-19 18:29:34 +0300

    remove all files to start a new challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_challenge/firebase_options.dart';
-import 'package:flutter_challenge/screens/auth.dart';
-import 'package:flutter_challenge/screens/chat.dart';
-import 'package:flutter_challenge/screens/splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
-  // Since I made main async, I need to call WidgetsFlutterBinding.ensureInitialized()
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -25,19 +16,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const SplashScreen();
-          }
-          if (snapshot.hasData) {
-            return const ChatScreen();
-          } else {
-            return const AuthScreen();
-          }
-        },
-      ),
+      home: const Scaffold(),
     );
   }
 }
